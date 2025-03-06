07a446f62d0a4da9301b6e8c1a7ccfced74390a0
user <user@192.168.10.8>
2025-03-06 12:42:08 +0500

    Implemented Django Logging with Loguru

--- api/config/settings/base.py
+++ api/config/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 from os import getenv, path 
+from loguru import logger
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -98,8 +99,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': getenv("POSTGRES_DB"),
+        'USER': getenv("POSTGRES_USER"),
+        "PASSWORD": getenv("POSTGRES_PASSWORD"),
+        "HOST": getenv("POSTGRES_HOST"),
+        "PORT": getenv("POSTGRES_PORT"),
+
     }
 }
 
@@ -155,3 +161,43 @@ STATIC_ROOT = str(BASE_DIR / "staticfiles")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING_CONFIG = None
+LOGURU_LOGGING = {
+    "handlers":[
+    {
+        "sink": BASE_DIR /"logs/debug.log",
+        "level": "DEBUG",
+        "filter": lambda record: record["level"].no <= logger.level("WARNING").no,
+        "format" : "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}: {function}:{line} - {message}",
+        "rotation": "10MB",
+        "retention": "30 days",
+        "compression": "zip",
+
+
+    },
+    {
+        "sink": BASE_DIR /"logs/error.log",
+        "level": "ERROR",
+        "format" : "{time:YYYY-MM-DD HH:mm:ss.SSS} | {level: <8} | {name}: {function}:{line} - {message}",
+        "rotation": "10MB",
+        "retention": "30 days",
+        "compression": "zip",
+        "backtrace": True,
+        "diagnose": True,
+        
+
+    }
+    
+    ],
+
+}
+logger.configure(**LOGURU_LOGGING)
+
+LOGGING = {
+    "version": "1",
+    "disable_existing_loggers": False,
+    "handlers": {"loguru": {"class": "interceptor.InterceptHandler"}},
+    "root" :{"handlers":["loguru"], "level": "DEBUG"}
+}
