fd22b323c181800b8d8c61b3a23b4894d90245e5
user <user@192.168.10.8>
2025-03-08 23:29:07 +0500

    Created Custom Django User Model, configured OTP, superuser, Spectacular Docs, and Custom Admin

--- api/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.contrib import admin
-from django.urls import path
-from core_apps.user_auth.views import TestLoggingView
-
-urlpatterns = [
-    path(settings.ADMIN_URL, admin.site.urls),
-    
-]
